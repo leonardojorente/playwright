40cee6cc037550256caa6c39e3dbf96fb9eed9ef
ljorente <ljorente@fattail.com>
2024-10-18 14:19:39 -0300

    update

--- tests/support/global-setup.ts
+++ tests/support/global-setup.ts
@@ -1,13 +1,7 @@
-import  LoginPayload  from '../data/request-payloads/post-signin-payload.json';
-
-LoginPayload.email = process.env.USER!
-LoginPayload.senha = process.env.PASSWORD!
 
 async function globalSetup() {
 
-  process.env.TEST = 'test';
-  console.log(`vida ${process.env.API_TOKEN}`);
-  console.log(`vida ${process.env.TEST}`);
+  console.log(`works as before all hook, only one function, a place to set process.env values`)
 }
 
 export default globalSetup;
